ee348924dbe67fe5255b3090eafeca1699682ad9
Fredrik Adelöw <freben@gmail.com>
2020-06-26 09:41:28 +0200

    feat(test-utils): mock storage api

---
+++ packages/test-utils/src/testUtils/apis/StorageApi/MockStorageApi.ts
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Observable,
+  StorageApi,
+  storageApiRef,
+  StorageValueChange,
+} from '@backstage/core-api';
+import ObservableImpl from 'zen-observable';
+
+export class MockStorageApi implements StorageApi {
+  static factory = {
+    implements: storageApiRef,
+    deps: {},
+    factory: () => new MockStorageApi(''),
+  };
+
+  private readonly namespace: string;
+  private readonly data: Map<string, any>;
+
+  constructor(namespace: string, data?: Map<string, any>) {
+    this.namespace = namespace;
+    this.data = data ?? new Map();
+  }
+
+  forBucket(name: string): StorageApi {
+    return new MockStorageApi(`${this.namespace}/${name}`, this.data);
+  }
+
+  get<T>(key: string): T | undefined {
+    return this.data.get(this.getKeyName(key));
+  }
+
+  async set<T>(key: string, data: T): Promise<void> {
+    this.data.set(this.getKeyName(key), data);
+    this.notifyChanges({ key, newValue: data });
+  }
+
+  async remove(key: string): Promise<void> {
+    this.data.delete(this.getKeyName(key));
+    this.notifyChanges({ key, newValue: undefined });
+  }
+
+  observe$<T>(key: string): Observable<StorageValueChange<T>> {
+    return this.observable.filter(({ key: messageKey }) => messageKey === key);
+  }
+
+  private getKeyName(key: string) {
+    return `${this.namespace}/${encodeURIComponent(key)}`;
+  }
+
+  private notifyChanges<T>(message: StorageValueChange<T>) {
+    for (const subscription of this.subscribers) {
+      subscription.next(message);
+    }
+  }
+
+  private subscribers = new Set<
+    ZenObservable.SubscriptionObserver<StorageValueChange>
+  >();
+
+  private readonly observable = new ObservableImpl<StorageValueChange>(
+    subscriber => {
+      this.subscribers.add(subscriber);
+      return () => {
+        this.subscribers.delete(subscriber);
+      };
+    },
+  );
+}
--- packages/test-utils/src/testUtils/apis/StorageApi/index.ts
+++ packages/test-utils/src/testUtils/apis/StorageApi/index.ts
@@ -14,14 +14,4 @@
  * limitations under the License.
  */
 
-import { ApiTestRegistry } from '@backstage/core-api';
-import { MockErrorApi, MockStorageApi } from './apis';
-
-export function createMockApiRegistry(): ApiTestRegistry {
-  const registry = new ApiTestRegistry();
-
-  registry.register(MockErrorApi.factory);
-  registry.register(MockStorageApi.factory);
-
-  return registry;
-}
+export { MockStorageApi } from './MockStorageApi';
